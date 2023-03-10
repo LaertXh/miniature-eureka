b1fb50e9a366ef5a31a11a34bf832e8c9888e34e
Laert Xhumari <laertxhumari@gmail.com>
2023-03-10 17:04:13 -0500

    created the server app

--- Develop/server.js
+++ Develop/server.js
@@ -1,2 +1,12 @@
+// DEPENDENCIES
 const apiRouts = require("./routes/api.js");
 const htmlRouts = require("./routes/html.js");
+
+const express = require("express");
+
+//create app and a custom port;
+const app = express();
+const port = process.env.PORT || 4000;
+
+// start listening on the port
+app.listen(port, () => console.log("App started"));
