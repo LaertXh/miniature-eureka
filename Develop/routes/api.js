e6d29e1dad8cb65b9b632f064c1ee644211ca878
Laert Xhumari <laertxhumari@gmail.com>
2023-03-11 11:37:06 -0500

    completed the api route

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,4 +1,15 @@
 const api = require("express").Router();
 const { getParsedStr, addData } = require("../interactions/write");
 
+api.get("/notes", (req, res) => {
+  getParsedStr().then((data) => {
+    return res.json(data);
+  });
+});
+
+api.post("/notes", (req, res) => {
+  addData(req.body);
+  res.json("New Data Logged");
+});
+
 module.exports = api;
