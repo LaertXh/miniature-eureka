c200f75413d5b28026dab552f3e64981c3b0de67
Laert Xhumari <laertxhumari@gmail.com>
2023-03-12 17:09:10 -0400

    fixed html paths

--- routes/html.js
+++ routes/html.js
@@ -3,12 +3,12 @@ const path = require("path");
 
 // GET Route for homepage
 Router.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+  res.sendFile(path.join(__dirname, "../public/index.html"))
 );
 
 // GET Route for feedback page
 Router.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
+  res.sendFile(path.join(__dirname, "../public/notes.html"))
 );
 
 module.exports = Router;
